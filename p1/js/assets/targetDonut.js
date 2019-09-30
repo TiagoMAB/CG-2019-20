22d07fb211a0ff53fdca7462a3d0ccf4186fabe8
TiagoMAB <tiago.barroso1@hotmail.com>
2019-09-30 20:45:28 +0100

    only some minor details missing

--- p1/js/assets/targetDonut.js
+++ p1/js/assets/targetDonut.js
@@ -4,7 +4,7 @@ class Toroid extends THREE.Object3D {
         this.material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
         this.geometry = new THREE.TorusGeometry(2, 0.3, 16, 32);
         this.mesh = new THREE.Mesh(this.geometry, this.material);
-
+        
         this.add(this.mesh);
         this.position.set(x, y, z);
     }
